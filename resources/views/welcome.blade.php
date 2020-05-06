e2e510c2243322cbd1fdf88e84159834b6b3773c
arthur.de.groote <arthur.de.groote@student.howest.be>
2020-05-06 15:31:19 +0200

    made the main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/reset.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('css/stylesheet.css') }}" />
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
     <title>Purr-palace</title>
 </head>
 <body>
-    <p>hi</p>
+    <header>
+        <img src="{{ asset('/images/logo.png') }}"/>
+        <h1>purr-palace</h1>
+        
+    </header>
+
+    <main>
+        <section>
+        <p>Choose you're arangement today!</p>
+        <img src=" {{ asset('/images/theme_image.jpg') }}"/>
+        </section>
+        <section>
+        @yield("main")
+        </section>
+    </main>
+    <footer>
+        &copy; Arthur De Groote
+    </footer>
+
+    
 </body>
 </html>
